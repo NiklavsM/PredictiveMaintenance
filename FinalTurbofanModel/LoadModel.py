304725d852065bc2dbf70ce28ad259524ce3f95c
Aidan Bonnar <aidan.bonnar1@gmail.com>
2020-02-23 16:14:48 +0000

    fixing the load model script

--- FinalTurbofanModel/LoadModel.py
+++ FinalTurbofanModel/LoadModel.py
@@ -8,7 +8,8 @@ from sklearn.decomposition import PCA
 import numpy as np
 import json
 
-print("Hey")
+# To just use the getPrediction method write 'import all' in your environment
+# To see the predictList as well call 'from LoadModel import *'
 
 
 json_file = open('savedStates/model.json', 'r')
@@ -26,9 +27,11 @@ scaler = pickle.load(open("savedStates/scaler.p", "rb"))
 
 pca = pickle.load(open("savedStates/pca.p", "rb"))
 
+print("Loaded Scaler and PCA")
+
 predictList = []
 
-
+# Will return a 1-d, empty array if not enough lines yet, will return 1-d, 1-element array if it has enough lines(30 lines required for prediction)
 def getPrediction(line) :
     line = line["instances"]
     print(line)
@@ -47,4 +50,4 @@ def getPrediction(line) :
         
         finalArray = np.array([reduced])
         result = loaded_model.predict(finalArray)
-        return result
+        return result[0]
