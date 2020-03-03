c76e0d5c41b3e9797526b067246d30ddaafd2bdd
Gordon Clark <33608980+gmc112@users.noreply.github.com>
2020-03-03 00:26:42 +0000

    Setup database for reduced data

--- ModelDepoyment/server/src/main/java/com/tensorflow/model/server/Repository/NasaPCARepository.java
+++ ModelDepoyment/server/src/main/java/com/tensorflow/model/server/Repository/NasaPCARepository.java
@@ -1,14 +1,11 @@
 package com.tensorflow.model.server.Repository;
 
-import com.tensorflow.model.server.DAO.Nasa;
+import com.tensorflow.model.server.DAO.NasaPCA;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface NasaRepository extends JpaRepository<Nasa, String> {
-
-    Nasa findById(@Param("id") Integer id);
+public interface NasaPCARepository extends JpaRepository<NasaPCA, String> {
+    NasaPCA findById(@Param("id") Integer id);
 }
-
--- ModelDepoyment/server/src/main/java/com/tensorflow/model/server/controllers/DataController.java
+++ ModelDepoyment/server/src/main/java/com/tensorflow/model/server/controllers/DataController.java
@@ -1,10 +1,15 @@
 package com.tensorflow.model.server.controllers;
 
 import com.tensorflow.model.server.DAO.Nasa;
+import com.tensorflow.model.server.DAO.NasaPCA;
+import com.tensorflow.model.server.Service.NasaPCAService;
 import com.tensorflow.model.server.Service.NasaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/data")
@@ -12,7 +17,13 @@ class DataController {
 
     @Autowired
     private NasaService nasaService;
+
+    @Autowired
+    private NasaPCAService nasaPCAService;
+
     private int nextId = 0;
+
+    private int nextPCA = 0;
 
     @GetMapping(value = "/nasa/",
                 produces = MediaType.APPLICATION_JSON_VALUE)
@@ -22,5 +33,16 @@ class DataController {
         return out;
     }
 
+    // ToDo rework to 30 rows at once
+    @GetMapping(value="/nasapca/",
+                produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<NasaPCA> findLatestThirtyRows(){
+        List<NasaPCA> rows = new LinkedList<>();
+        for (int i = 0; i < 30; i++){
+            rows.add(nasaPCAService.findById(nextPCA));
+            nextPCA += 1;
+        }
+        return rows;
+    }
 
 }
