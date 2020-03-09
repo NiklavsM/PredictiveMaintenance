866a2b368871fe79528a22c4f89a2b1ee473a80b
NiklavsM <niklavs.meiers@gmail.com>
2020-03-09 14:06:38 +0000

    Got rid of unused client. Parsed the 30 rows.

--- ModelDepoyment/server/src/main/java/com/tensorflow/model/server/controllers/PredictionController.java
+++ ModelDepoyment/server/src/main/java/com/tensorflow/model/server/controllers/PredictionController.java
@@ -1,8 +1,11 @@
 package com.tensorflow.model.server.controllers;
 
+import com.tensorflow.model.server.DAO.NasaPCA;
+import com.tensorflow.model.server.Service.NasaPCAService;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -12,10 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 class PredictionController {
+
+    @Autowired
+    private NasaPCAService nasaPCAService;
+    private int nextPCA = 0;
 
     @GetMapping("/predict")
     public String predict() {
@@ -25,38 +34,8 @@ class PredictionController {
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         JSONObject sensoryData = new JSONObject();
 
-        double[][][] array = {{{0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561},
-                {0.741379, 0.500000, 0.370482, 0.285154, 0.408001, 1.0, 0.679549, 0.196970, 0.105717, 0.255952, 0.573561}}};
         try {
-            sensoryData.put("instances", new JSONArray(array));
+            sensoryData.put("instances", new JSONArray(parse30Rows()));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -66,8 +45,40 @@ class PredictionController {
         RestTemplate rt = new RestTemplate();
         String url = "http://localhost:8501/v1/models/predictive_maintenance:predict";
         String response = rt.postForObject(url, request, String.class);
+        findLatestThirtyRows();
+
         System.out.println("response " + response);
         return response;
+    }
+
+    //TODO join this with DataController
+    public List<NasaPCA> findLatestThirtyRows() {
+        List<NasaPCA> rows = new LinkedList<>();
+        for (int i = 0; i < 30; i++) {
+            rows.add(nasaPCAService.findById(nextPCA));
+            nextPCA += 1;
+        }
+        return rows;
+    }
+
+    private double[][][] parse30Rows() {
+        double[][][] array = new double[1][30][11];
+        List<NasaPCA> rows = findLatestThirtyRows();
+        int index = 0;
+        for (NasaPCA row : rows) {
+            array[0][index][0] = row.getS1();
+            array[0][index][1] = row.getS2();
+            array[0][index][2] = row.getS3();
+            array[0][index][3] = row.getS4();
+            array[0][index][4] = row.getS5();
+            array[0][index][5] = row.getS6();
+            array[0][index][6] = row.getS7();
+            array[0][index][7] = row.getS8();
+            array[0][index][8] = row.getS9();
+            array[0][index][9] = row.getS10();
+            array[0][index][10] = row.getS11();
+        }
+        return array;
     }
 
 }
