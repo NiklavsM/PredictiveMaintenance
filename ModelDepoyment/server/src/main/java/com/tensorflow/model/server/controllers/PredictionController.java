9da0e3bafa8f827c2e1841dfa887718c27bfb6b6
NiklavsM <niklavs.meiers@gmail.com>
2020-03-31 12:39:32 +0100

    Added deployment instructions

--- ModelDepoyment/server/src/main/java/com/tensorflow/model/server/controllers/PredictionController.java
+++ ModelDepoyment/server/src/main/java/com/tensorflow/model/server/controllers/PredictionController.java
@@ -29,10 +29,52 @@ class PredictionController {
     @GetMapping("/predict")
     public String predict() {
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        JSONObject sensoryData = new JSONObject();
+        try {
+            sensoryData.put("instances", new JSONArray(parse30Rows()));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        HttpEntity<String> request = new HttpEntity<String>(sensoryData.toString(), headers);
+        // create a map for post parameters
+        RestTemplate rt = new RestTemplate();
+        String url = "http://localhost:8501/v1/models/predictive_maintenance:predict";
+        String response = rt.postForObject(url, request, String.class);
+        findLatestThirtyRows();
 
-        return "{ \"predictions\": [[82.9299545]]}";
+        return response;
     }
 
+    public List<NasaPCA> findLatestThirtyRows() {
+        List<NasaPCA> rows = new LinkedList<>();
+        for (int i = 0; i < 30; i++) {
+            rows.add(nasaPCAService.findById(nextPCA + i));
+        }
+        nextPCA += 1;
+        return rows;
+    }
 
-
+    private double[][][] parse30Rows() {
+        double[][][] array = new double[1][30][11];
+        List<NasaPCA> rows = findLatestThirtyRows();
+        int index = 0;
+        for (NasaPCA row : rows) {
+            array[0][index][0] = row.getS1();
+            array[0][index][1] = row.getS2();
+            array[0][index][2] = row.getS3();
+            array[0][index][3] = row.getS4();
+            array[0][index][4] = row.getS5();
+            array[0][index][5] = row.getS6();
+            array[0][index][6] = row.getS7();
+            array[0][index][7] = row.getS8();
+            array[0][index][8] = row.getS9();
+            array[0][index][9] = row.getS10();
+            array[0][index][10] = row.getS11();
+            index++;
+        }
+        return array;
+    }
 }
---
+++ readme.md
@@ -0,0 +1,23 @@
+#Deployment instructions
+
+##System Requirements
+
+* Docker installed
+* Maven installed
+* Git installed
+* Npm installed
+
+## Deploying the trained model
+
+* TensorFlow serving
+    * Clone https://github.com/tensorflow/serving
+    * Create a folder and put the model to be deployed under /serving/tensorflow_serving/servables/tensorflow/testdata/
+    * Run command : docker run -t --rm -p 8501:8501     -v "/$(pwd)/serving/tensorflow_serving/servables/tensorflow/testdata/*Model_directory_name*:/models/*Model_name*" -e MODEL_NAME=*Model_name*     tensorflow/serving &
+* Spring boot
+    * Navigate to the directory ModelDeployment/server
+    * Run "mvn install"
+    * To start the server run "mvn spring-boot:run"
+* React client
+    * Navigate to the directory client/
+    * Run "npm install"
+    * To start the client run "npm start"
